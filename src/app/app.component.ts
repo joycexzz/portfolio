af771f55f6d6ac7980818b947c1ee2b7d8d65a78
joycexzz <joycexzz18@gmail.com>
2024-02-08 02:41:08 +0800

    removed aos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', 'node_modules/aos/dist/aos.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('1s ease-in')]),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'portfolio';
+
+  scrollAnimationState: boolean = false;
+
+  constructor() {
+    
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    // You'd check the position of your element and set `scrollAnimationState` to true if it's in view.
+    // For demonstration, let's say it's always true when scrolling
+    this.scrollAnimationState = true;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/app.module';
-import AOS from 'aos';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(() => {
-    AOS.init();
-  })
   .catch(err => console.error(err));
